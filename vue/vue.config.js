ba7021be287a221ba53346a1a19c2db5288d7717
xudong7 <13433126742@163.com>
2024-12-14 22:31:10 +0800

    update webConfig

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
-      '/': {
+      '/**': {
         target: 'http://localhost:8080',  // 设置后端 Spring Boot 的 API 地址
         changeOrigin: true,               // 为虚拟主机做适配
         secure: false                     // 如果后端没有使用 https，设置为 false
